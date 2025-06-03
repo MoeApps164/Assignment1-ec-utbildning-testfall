17e1b2d293546f74cfab69aeb519afc9f22be760
Mohammad Kaldi <Mohammed.kaldi@outlook.com>
2025-06-04 00:58:45 +0200

    Merge remote-tracking branch 'origin/master'  Del 3                        Kommentar    lagts till för allt                                               Vidareutveckla ditt program i del 2 så att programmet även gör följande:

--- assignment1continuepart2/Main.java
+++ assignment1continuepart2/Main.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
 
+
+
+
 public class Main {
+    //Programmet börjar här i main
+
 
     public static void main(String[] args) {
+        // gör det möjligt att ta in input
         Scanner scan = new Scanner(System.in);
+        //skapar ett nytt objekt av textstatistik så vi kan sätta input atrtibuter i den
         TextStatistik statistik = new TextStatistik();
 
         System.out.println("Skriv text (stop för att avsluta):");
+        // gör en ny rad
         String input = scan.nextLine();
-
-        while (!statistik.ärStop(input)) { // Flyttad kontroll hit
+//logiken att programmet ska loopa tills ordet stop skrivs in i input om inte
+        //lägg till en ny rad i listan och gå till nästa rad
+        while (!statistik.ärStop(input)) {
             statistik.läggTillRad(input);
             input = scan.nextLine();
         }
-
+//skriver ut de totala av allt
         System.out.println("Antal rader: " + statistik.getRadAntal());
         System.out.println("Antal tecken: " + statistik.getTeckenAntal());
         System.out.println("Antal ord: " + statistik.getOrdAntal());
--- assignment1continuepart2/TextStatistik.java
+++ assignment1continuepart2/TextStatistik.java
@@ -1,44 +1,49 @@
 public class TextStatistik {
-
+//atributer
     private int radAntal = 0;
     private int teckenAntal = 0;
     private int ordAntal = 0;
     private String längstaOrdet = "";
-
+//metod
     public void läggTillRad(String rad) {
-        // Räknar rad
+        // Räknar antalet rader
+
         radAntal++;
+        //räknar längden på raden
         teckenAntal += rad.length();
 
-        // Räkna ord (separerade med blanksteg)
+        // Räkna ord (separerade med space)
         String[] ord = rad.split(" ");
+        //räknar hur många ord det är i  raden och lägger sen till det i totala
         ordAntal += ord.length;
 
-        // Kolla om det finns ett längre ord än nuvarande
+        // loopar igenom alla ord i raden  om något ord är längre än det längsta
+        //byts den nya mot det gamla längsta.
         for (String ettOrd : ord) {
             if (ettOrd.length() > längstaOrdet.length()) {
                 längstaOrdet = ettOrd;
             }
         }
     }
-
+//returnerar hur många rader som lagts till
     public int getRadAntal() {
         return radAntal;
     }
-
+    //returnerar totala antalet tecken, alltså varje tangenttryck totala.
     public int getTeckenAntal() {
         return teckenAntal;
     }
-
+//returnerar antal ord hela ord tills man trycker space
     public int getOrdAntal() {
         return ordAntal;
     }
-
+//returnerar längsta ordet
     public String getLängstaOrdet() {
         return längstaOrdet;
     }
 
     // Flytta kontrollen för "stop" hit (istället för Main)
+    //om användarens ord är lika med stop slutar programmet
     public boolean ärStop(String rad) {
         return rad.equalsIgnoreCase("stop");
     }
