45490a06de095692b6036f8e884e945d1e3d655a
Mohammad Kaldi <Mohammed.kaldi@outlook.com>
2025-06-04 00:58:45 +0200

    Merge remote-tracking branch 'origin/master'  Del 3                        Kommentar    lagts till för allt                                               Vidareutveckla ditt program i del 2 så att programmet även gör följande:

--- assignment1continuepart2/TextStatistikTest.java
+++ assignment1continuepart2/TextStatistikTest.java
@@ -2,27 +2,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextStatistikTest {
-
+//detta är test filen @Test säger till programmet att använda org.junit. som vi importerat
     @Test
+    //skapar ny objekt utav textstatistik  och sedan sätter vi själva värden för testa
+    //i detta fall läggtill rad och radantalet ska vara 1 för vara korekt
     public void testAttRadRäknas() {
         TextStatistik ts = new TextStatistik();
         ts.läggTillRad("Hej");
         assertEquals(1, ts.getRadAntal());
     }
-
+// i denna testar vi teckenantal där teckenantal ska vbara 6 altså ord samanlagt
+    //man tryckt i tangentbordet
     @Test
     public void testAttTeckenRäknas() {
         TextStatistik ts = new TextStatistik();
         ts.läggTillRad("Hej"); // 3 tecken
         ts.läggTillRad("Där"); // 3 till
         assertEquals(6, ts.getTeckenAntal());
+
     }
+    //testar att inget händer alltså 0 utav 0 ingen atribut läggs in
     @Test
     public void testAttIngetHänderUtanRad() {
         TextStatistik ts = new TextStatistik();
         assertEquals(0, ts.getRadAntal());
         assertEquals(0, ts.getTeckenAntal());
     }
+    // test om ordantal visar korakt exempel hej ali   räknas som 2 ord
     @Test
     public void testOrdAntalRäknasKorrekt() {
         TextStatistik ts = new TextStatistik();
